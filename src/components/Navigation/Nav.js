d6b42f337f855d356b2c407570a401dd97b2bf02
TheLoyalLnk <77541532+gaminglnk@users.noreply.github.com>
2023-05-10 22:25:38 +0530

    Search btn accessible.

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -34,7 +34,7 @@ function Nav() {
               },
             }}
           >
-            <Button onClick={(e) => setIsActive(!isActive)}>
+            <Button id="search-btn" onClick={(e) => setIsActive(!isActive)}>
               <FiSearch />
             </Button>
           </IconContext.Provider>
@@ -50,7 +50,7 @@ function Nav() {
               },
             }}
           >
-            <Button onClick={(e) => setIsActive(!isActive)}>
+            <Button id="search-btn" onClick={(e) => setIsActive(!isActive)}>
               <FiSearch />
               Search
             </Button>
