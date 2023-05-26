b7ef38a26ca4d68d1a13585c459d3c083c462db7
TheLoyalLnk <77541532+gaminglnk@users.noreply.github.com>
2023-05-26 13:26:16 +0530

    Update Footer.js

--- src/components/Universal/Footer.js
+++ src/components/Universal/Footer.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const FooterContainer = styled.footer`
   background-color: #0c0d10;
@@ -10,130 +10,63 @@ const FooterContainer = styled.footer`
   flex-direction: column;
   align-items: center;
   text-align: center;
-
-  @media (min-width: 600px) {
-    padding: 2rem 5rem;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-start;
-  }
-`;
-
-const LogoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-
-  @media (min-width: 600px) {
-    gap: 1rem;
-  }
 `;
 
 const LogoText = styled.h1`
-  font-family: 'Lexend', sans-serif;
-  font-size: 2rem;
-  margin: 0;
+  font-family: "Lexend", sans-serif;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
 `;
 
-const ContentContainer = styled.div`
-  margin-top: 2rem;
-
-  @media (min-width: 600px) {
-    margin-top: 0;
-  }
-`;
-
-const InfoText = styled.p`
-  font-family: 'Lexend', sans-serif;
+const Description = styled.p`
+  font-family: "Lexend", sans-serif;
   font-size: 0.9rem;
   color: #9c9c9c;
-  margin: 1rem 0;
-
-  @media (min-width: 600px) {
-    font-size: 1rem;
-  }
+  margin-bottom: 2rem;
 `;
 
-const NavContainer = styled.div`
-  margin-top: 2rem;
+const FootLinks = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
   gap: 1.5rem;
+  margin-bottom: 1rem;
 
-  @media (min-width: 600px) {
-    flex-direction: row;
-    align-items: flex-start;
-    gap: 5rem;
-    margin-top: 0;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    gap: 0.5rem;
   }
 `;
 
-const NavList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-`;
-
-const NavItem = styled.li`
-  font-family: 'Lexend', sans-serif;
-`;
-
-const NavLink = styled(Link)`
+const FootLink = styled(Link)`
   color: #a7a7a7;
   text-decoration: none;
+  transition: color 0.3s ease-in-out;
 
   &:hover {
     color: #ffffff;
   }
 `;
 
-function Footer() {
-  const [year] = useState(new Date().getFullYear());
-
+const Footer = () => {
   return (
     <FooterContainer>
-      <LogoContainer>
-        <LogoText>kimitsu</LogoText>
-      </LogoContainer>
-      <ContentContainer>
-        <InfoText>
-          &copy; {year} kimitsu. | React app made with ❤️
-        </InfoText>
-        <InfoText>
-          This site does not store any files on our server, we only link to
-          the media which is hosted on 3rd party services.
-        </InfoText>
-      </ContentContainer>
-      <NavContainer>
-        <NavList>
-          <NavItem>
-            <NavLink to="/popular/1">Popular Anime</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/genre/Action">Action Anime</NavLink>
-          </NavItem>
-        </NavList>
-        <NavList>
-          <NavItem>
-            <NavLink to="/movies/1">Movies</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/trending/1">Latest Shows</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/dmca">DMCA</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/gaminglnk/">Github</NavLink>
-          </NavItem>
-        </NavList>
-      </NavContainer>
+      <LogoText>kimitsu</LogoText>
+      <Description>
+        &copy; {new Date().getFullYear()} kimitsu. | React app made with ❤️
+      </Description>
+      <FootLinks>
+        <FootLink to="/popular/1">Popular Anime</FootLink>
+        <FootLink to="/genre/Action">Action Anime</FootLink>
+        <FootLink to="/movies/1">Movies</FootLink>
+        <FootLink to="/trending/1">Latest Shows</FootLink>
+        <FootLink to="/dmca">DMCA</FootLink>
+        <FootLink href="https://github.com/gaminglnk/">Github</FootLink>
+      </FootLinks>
+      <Description>
+        This site does not store any files on our server, we only link to the
+        media which is hosted on 3rd party services.
+      </Description>
     </FooterContainer>
   );
-}
+};
 
 export default Footer;
