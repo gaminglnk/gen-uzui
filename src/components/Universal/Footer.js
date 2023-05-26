a11ac37c40331aa23bc5c39c928cdd0f98ca8047
TheLoyalLnk <77541532+gaminglnk@users.noreply.github.com>
2023-05-26 13:59:15 +0530

    Update Footer.js

--- src/components/Universal/Footer.js
+++ src/components/Universal/Footer.js
@@ -44,6 +44,7 @@ const FootLink = styled(Link)`
 
 const Footer = () => {
   return (
+    <div style={{ height: "1px", backgroundColor: "#393653", borderRadius: "2px", margin: "1.5rem 0", }} ></div>
     <FooterContainer>
       <LogoText>kimitsu</LogoText>
       <Description>
